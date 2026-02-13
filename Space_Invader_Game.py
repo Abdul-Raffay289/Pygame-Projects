31ae0cb6ec3fa2f1374b9de9762ece1aee87bf10
Abdul-Raffay289 <abdullraffay289@gmail.com>
2026-02-13 19:28:45 +0500

    Space_Invader_Game

--- Space_Invader_Game.py
+++ Space_Invader_Game.py
@@ -50,3 +50,70 @@ for i in range(numbers_of_enemies):
     enemyY.append(random.randint(ENEMY_START_Y_MIN, ENEMY_START_Y_MAX))
     enemyX_change.append(ENEMY_SPEED_X)
     enemyY_change.append(ENEMY_SPEED_Y)
+
+# Bullet
+bulletIMG = pygame.load.image('Bullet.png')
+bullet_X = 0
+bullet_y = PLAYER_START_Y
+bullet_X_change = 0
+bullet_Y_change = BULLET_SPEED_Y
+bullet_state = "ready"
+
+# Score
+score_value = 0
+font = pygame.font.Font("freesonbold.ttf", 32)
+textX = 10
+textY = 10
+
+# Game over text
+over_font = pygame.font.Font("freesonbold.ttf", 64)
+
+def show_score(x, y):
+    score = pygame.render("score" +str(score_value), True, (255, 255, 255))
+    screen.blit(score, (x, y))
+
+def game_over_text():
+    score = pygame.render("Game Over", True, (255, 255, 255))
+    screen.blit(score, (200, 200))
+
+def player(x, y):
+    # Draw the player on the screen
+    screen.blit(playerImg, (x, y))
+
+def enemy(x, y, i):
+    # Draw an enemy on the screen
+    screen.blit(enemyImg[i], (x, y))
+
+def fire_bullet(x, y):
+    # Fire a bullet from the player's position
+    global bullet_state
+    bullet_state = "fire"
+    screen.blit(bulletImg, (x + 16, y + 10))
+
+def isCollision(enemyX, enemyY, bulletX, bulletY):
+    # Check if there is a collision between the enemy and a bullet
+    distance = math.sqrt((enemyX - bulletX) ** 2 + (enemyY - bulletY) ** 2)
+    return distance < COLLISION_DISTANCE
+
+# Game Loop
+running = True
+while running:
+    scree.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running == False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -5
+
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 5
+            if event.key == pygame.K_SPACE and bullet_state == "ready":
+                bullet_X = playerX
+                fire_bullet(bullet_X, bullet_y)
+        if event.type == pygame.KEYUP and event.key in [pygame.K_LEFT, pygame.K_RIGHT]:
+            playerX_change = 0
+        # Player Movement
+        playerX += playerX_change
+        playerX = max(0, min(playerX, SCREEN_WIDTH - 64))  # 64 is the size of the player
